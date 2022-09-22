0ceddfc582f24c3e7a0659b174c1002d5af728c7
takna <tak_na@icloud.com>
2022-09-22 23:43:14 +0900

    getStaticProps 関数を追加、Home 関数で受け取り表示させるテスト (ブラウザコンソールで確認)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-export default function Home() {
+export default function Home(props) {
+  console.log("PROPS: ", props);
   return <div>Next JS &amp; WordPress course.</div>;
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      myexampleprop: "test"
+    }
+  }
 }
