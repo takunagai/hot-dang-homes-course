8831c9a2f509798ebaa63aa486e408bd70b9d191
takna <tak_na@icloud.com>
2022-09-23 11:40:54 +0900

    WordPress GraphQL エンドポイントからページタイトルを取得しコンソールに出力

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import client from "client";
+import { gql } from "@apollo/client";
+
 export default function Home(props) {
   console.log("PROPS: ", props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
 
 export const getStaticProps = async () => {
+  const {data} = await client.query({
+    query: gql`
+      query NewQuery {
+        pages {
+          nodes {
+            title
+          }
+        }
+      }
+    `
+  })
   return {
     props: {
+      data,
       myexampleprop: "test"
     }
   }
