8961ca268d7b56e3787e7a74c9bb34070b7351f2
takna <tak_na@icloud.com>
2022-09-26 04:54:10 +0900

    Heading コンポーネントを作成

--- components/BlockRenderer/BlockRenderer.tsx
+++ components/BlockRenderer/BlockRenderer.tsx
@@ -1,4 +1,5 @@
 import { Cover } from "../Cover";
+import { Heading} from "../Heading";
 
 type Props = {
   blocks: {
@@ -10,6 +11,9 @@ type Props = {
 export const BlockRenderer = ({ blocks }: Props) => {
   return blocks.map(block => {
     switch(block.name) {
+      case 'core/heading': {
+        return <Heading key={block.id} />;
+      }
       case 'core/cover': {
         console.log("BLOCK", block); // 取得したデータ確認用
         return <Cover key={block.id} background={block.attributes.url}>core cover</Cover>;
