053b12b353ad6e34e3e179dc2125f1ed62a1c42f
takna <tak_na@icloud.com>
2022-09-26 08:52:31 +0900

    Heading コンポーネントに属性値を渡すように / 型付け(未解決)

--- components/BlockRenderer/BlockRenderer.tsx
+++ components/BlockRenderer/BlockRenderer.tsx
@@ -1,24 +1,40 @@
 import { Cover } from "../Cover";
-import { Heading} from "../Heading";
+import { Heading } from "../Heading";
+import type { FunctionComponent } from 'react';
 
 type Props = {
-  blocks: {
-    id: string,
-    name: string
-  }
-}
+  blocks: [
+    {
+      id: string,
+      name: string,
+      attributes: {
+        content: string,
+        level: number,
+        textAlign: string,
+      },
+    }
+  ]
+};
 
-export const BlockRenderer = ({ blocks }: Props) => {
+// ★★TODO: この関数(中で同関数の再呼び出しあり)の型定義、正しくは？
+export const BlockRenderer: FunctionComponent = ({ blocks }: Props) => {
   return blocks.map(block => {
     switch(block.name) {
       case 'core/heading': {
-        return <Heading key={block.id} />;
+        return <Heading
+          key={block.id}
+          content={block.attributes.content}
+          level={block.attributes.level}
+          textAlign={block.attributes.textAlign}
+        />;
       }
       case 'core/cover': {
         console.log("BLOCK", block); // 取得したデータ確認用
-        return <Cover key={block.id} background={block.attributes.url}>
-          <BlockRenderer blocks={block.innerBlocks} />
-        </Cover>;
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
       }
       default:
         return null;
