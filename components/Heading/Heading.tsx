edf9dcc235c46c9e01637cb19617501bd423ffcb
takna <tak_na@icloud.com>
2022-09-29 15:50:30 +0900

    修正 (余分な } を削除)

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const Heading = ({ content, level, textAlign }: Props) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: {__html: content},
-    className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(level)} ${getTextAlign(textAlign)}}`
+    className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(level)} ${getTextAlign(textAlign)}`
   });
   return tag;
 };
