e19c7ad574c3f6462a4e1553cab0c150927b6d76
takna <tak_na@icloud.com>
2022-09-29 15:53:21 +0900

    固定ページ(子ページもあり)を表示するページコンポーネントを作成、ルーティング

---
+++ pages/[...slug].tsx
@@ -0,0 +1,38 @@
+import { gql } from "@apollo/client";
+import client from "client"
+
+export default function Page(props) {
+  console.log("PAGE PROPS: ", props); // 確認用。現時点では空オブジェクトだけ
+  return (
+    <div>page</div>
+  );
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths = async () => {
+  const {data} = await client.query({
+    query: gql`
+      query AllPagesQuery {
+        pages {
+          nodes {
+            uri
+          }
+        }
+      }
+    `
+  });
+
+  return {
+    paths: data.pages.nodes.map(page => ({
+      params: {
+        slug: page.uri.substring(1, page.uri.length - 1).split("/")
+      }
+    })),
+    fallback: false,
+  }
+};
