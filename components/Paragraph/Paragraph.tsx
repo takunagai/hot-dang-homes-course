4a1b16efe8caba16a51e7a4599c9d09cc5a75eb5
takna <tak_na@icloud.com>
2022-10-03 08:31:45 +0900

    Paragraph コンポーネントで行揃えスタイルが適用されるように

--- components/Paragraph/Paragraph.tsx
+++ components/Paragraph/Paragraph.tsx
@@ -1,7 +1,9 @@
-export const Paragraph = ({ textAlign, content, textColor }) => {
+import { getTextAlign } from "../../utils/fonts";
+
+export const Paragraph = ({ textAlign = "left", content, textColor }) => {
   return (
     <p
-      className="max-w-5xl mx-auto"
+      className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
       style={{ color: textColor }}
       dangerouslySetInnerHTML={{ __html: content }}
     />
