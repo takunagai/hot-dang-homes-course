96c8eac0046c822c338a00b68d27a7f1fbac9656
takna <tak_na@icloud.com>
2022-10-03 08:12:21 +0900

    WP theme.json のカラーパレットの役目をさせる設定ファイルを作成

---
+++ theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  foreground: "#000",
+  background: "#fff",
+  primary: "#1a4548",
+  secondary: "#ffe2c7",
+  tertiary: "#f6f6f6",
+  slate: "#1E293B",
+  'vivid-green-cyan': "#00d084"
+};
