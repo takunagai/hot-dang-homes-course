5c66af797aa4e6e0251cfcd1fb71b7ec4276ade9
takna <tak_na@icloud.com>
2022-10-04 15:55:57 +0900

    Paragraph コンポーネントに型付け

--- components/Paragraph/Paragraph.tsx
+++ components/Paragraph/Paragraph.tsx
@@ -1,6 +1,16 @@
 import { getTextAlign } from "../../utils/fonts";
 
-export const Paragraph = ({ textAlign = "left", content, textColor }) => {
+type Props = {
+  content: string,
+  textAlign: string,
+  textColor: string,
+}
+
+export const Paragraph = ({
+  content,
+  textAlign = "left",
+  textColor
+}: Props) => {
   return (
     <p
       className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
